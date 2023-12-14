96cc8655b14559800c895e5973fba01cfc88fca7
Seito Horiguchi <seito.horiguchi@gmail.com>
2023-12-14 23:49:39 +0900

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Login } from '@/components/login'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Login />      
+    </main>
+  )
+}
