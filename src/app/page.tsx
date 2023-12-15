bc96df3a9477bc5fee00d3aa542b267dc03cede3
Seito Horiguchi <seito.horiguchi@gmail.com>
2023-12-15 19:10:22 +0900

    added form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Login } from '@/components/login'
+import { ContactFrom } from '@/components/contact-from'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Login />      
+      <ContactFrom />
     </main>
   )
 }
+
